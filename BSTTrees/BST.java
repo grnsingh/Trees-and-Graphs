69bd9bc904d82c646e77d4e9388eaf2d0f7c181c
grnsingh <gurnoor.singh1@ucalgary.ca>
2023-06-01 00:00:42 -0600

    bst trees

---
+++ BSTTrees/BST.java
@@ -0,0 +1,17 @@
+package BSTTrees;
+
+public class BST {
+    private class Node{
+        private int data;
+        private Node left= null;
+        private Node right = null;
+
+        public Node(int data){
+            this.data = data;
+        }
+    }
+
+    public Node 
+
+    
+}
