db9331f8e7523b46e6e684b43b2021d8c575f514
grnsingh <gurnoor.singh1@ucalgary.ca>
2023-08-02 02:05:18 -0600

    Graph creation(4 vertices)

--- Graphs/Graphs.java
+++ Graphs/Graphs.java
@@ -1,5 +1,43 @@
 package Graphs;
 
+import java.util.*;
+
 public class Graphs {
-    
+
+    static class Edge{
+        int src;
+        int dest;
+
+        public Edge(int s, int d){
+            this.src= s;
+            this.dest = d;
+        }
+
+    }
+
+    public static void createGraph(ArrayList<Edge> graph[]){
+        // creating array of arraylists
+        for(int i =0; i<graph.length; i++){
+            graph[i] = new ArrayList<Edge>();
+        }
+        graph[0].add(new Edge(0,2));
+
+        graph[1].add(new Edge(1,2));
+        graph[1].add(new Edge(1,3));
+
+        graph[2].add(new Edge(2,0));
+        graph[2].add(new Edge(2,1));
+        graph[2].add(new Edge(2,3));
+
+        graph[3].add(new Edge(3,2));
+         graph[3].add(new Edge(3,1));
+    }
+    public static void main(String args[]){
+        int v=4;
+        // 4 vertices
+        ArrayList<Edge> graph[] = new ArrayList[v];
+        // Adjancey list of type array of ArrayList<Edge>
+
+    }
 }
+
