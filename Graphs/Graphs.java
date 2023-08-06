3fbbd4aef65aab2471110a2bcbdae603d167b2f3
grnsingh <gurnoor.singh1@ucalgary.ca>
2023-08-06 16:27:33 -0600

    implemented BFS

--- Graphs/Graphs.java
+++ Graphs/Graphs.java
@@ -31,9 +31,35 @@ public class Graphs {
         graph[3].add(new Edge(3,2));
         graph[3].add(new Edge(3,1));
     }
+
+    public static void bfs(ArrayList<Edge> graph[], int V){
+        Queue<Integer>  q = new LinkedList<>();
+        boolean visited[] = new boolean[V];
+
+        q.offer(0);
+        while(!q.isEmpty()){
+            int curr = q.poll();
+            if(visited[curr]==false){
+                System.out.println(curr);
+                visited[curr] = true;
+
+                for(int i=0; i<graph[curr].size(); i++){
+                    Edge e = graph[curr].get(i);
+                    q.offer(e.dest);
+                }
+            }
+
+        }
+
+    }
     public static void main(String args[]){
         int v=4;
         ArrayList<Edge> graph[] = new ArrayList[v];
+        // for(int i =0; i<graph[0].size();i++){
+        //     System.out.println(graph[0].get(i).dest);
+        // }
+        createGraph(graph);
+        bfs(graph, v);
 
     }
 }
